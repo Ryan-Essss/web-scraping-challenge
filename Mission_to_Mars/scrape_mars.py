086df0fbb4a389162cec6ccfc84775eed61a9df0
Ryan S <sabon.ryan@gmail.com>
2021-03-25 20:48:58 -0400

    add scrape/app

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import requests
+import pymongo
+import time
+from bs4 import BeautifulSoup
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+
